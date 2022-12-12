3cd6dc382bd6d0abc1f0782611ab3734f050337d
9663461085 <ratansollapure16310@gmail.com>
2022-12-12 12:11:03 +0530

    Added:welcome meassge to programm

---
+++ src/com/bridgelabz/BinarySearch.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+/***
+ * 
+ * @author Admin
+ *
+ */
+public class BinarySearch {
+	/***
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to binary search tree");
+	}
+}
