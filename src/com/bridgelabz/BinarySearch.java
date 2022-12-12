c644e42b9d8ec9791336b03dd8fb8e487c8955f8
9663461085 <ratansollapure16310@gmail.com>
2022-12-12 12:18:32 +0530

    Added:Created BTS By adding elements

--- src/com/bridgelabz/BinarySearch.java
+++ src/com/bridgelabz/BinarySearch.java
@@ -5,12 +5,39 @@ package com.bridgelabz;
  * @author Admin
  *
  */
-public class BinarySearch {
-	/***
-	 * 
-	 * @param args
-	 */
+public class BinarySearch<K extends Comparable<K>> {
+	 Node<K> root;
+    public void add(K key) {
+		this.root = this.insertData(root, key);
+	}
+
+	public Node<K> insertData(Node<K> current, K key) {
+		if (current == null)
+			return new Node<K>(key);
+		int compareResult = key.compareTo(current.key);
+
+		if (compareResult == 0)
+			return current;
+
+		if (compareResult < 0) {
+			current.left = insertData(current.left, key);
+		} else {
+			current.right = insertData(current.right, key);
+		}
+		return current;
+	}
+
+	@Override
+	public String toString() {
+		return "MyBinary [root=" + root + "]";
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to binary search tree");
+		BinarySearch<Integer> binaryTree = new BinarySearch<Integer>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+
+		System.out.println(binaryTree);
 	}
 }
